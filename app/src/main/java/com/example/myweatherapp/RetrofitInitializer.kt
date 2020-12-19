33793f3938c26a1e016028d27c80d3b84e22c290
thiagomonteiro03 <thiagomonteiro.trab@outlook.com>
2020-12-19 02:53:15 -0300

    adicionado retrofitInitializer ao projeto

---
+++ app/src/main/java/com/example/myweatherapp/RetrofitInitializer.kt
@@ -0,0 +1,20 @@
+package com.example.myweatherapp
+
+import com.google.gson.*
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
+
+class RetrofitInitializer {
+
+    fun getRetrofit() : Retrofit {
+
+        //TODO put your URL
+        return Retrofit.Builder().baseUrl("api.openweathermap.org/data/2.5/weather")
+                .addConverterFactory(GsonConverterFactory.create(GsonBuilder()
+                .registerTypeAdapter(Date::class.java, JsonDeserializer { json, typeOfT, context -> Date(json.asJsonPrimitive.asString.replace("\\D+".toRegex(), "").toLong()) }).create()))
+                .build()
+
+    }
+
+}
