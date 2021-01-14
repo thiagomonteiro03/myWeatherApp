270a9d78432df23aefbef16c456ada38d5fb7592
thiagomonteiro03 <thiagomonteiro.trab@outlook.com>
2021-01-14 00:18:53 -0300

    atualizando as models

--- app/src/main/java/com/example/myweatherapp/LocationWeatherModel.kt
+++ app/src/main/java/com/example/myweatherapp/LocationWeatherModel.kt
@@ -1,4 +1,20 @@
 package com.example.myweatherapp
 
-class LocationWeatherModel {
-}
+data class LocationWeatherModel(
+        var id: Int,
+        var name: String,
+        var country: String,
+        val lat: Double,
+        val lon: Double,
+        val threeHourlyRainVolume: Double,
+        var threeHourlySnowVolume: Double,
+        var windSpeed: Double?,
+        var weatherDescription: String?,
+        var temp: Int,
+        var tempMax: Int,
+        var tempMin: Int,
+        var humidity: Int,
+        var pressure: Int,
+        var feelsLike: Int,
+        var clouds: Int
+)
