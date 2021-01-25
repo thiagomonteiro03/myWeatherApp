060213f36dbfa0d23abdb60678db87ef3c47ebf2
thiagomonteiro03 <thiagomonteiro.trab@outlook.com>
2021-01-25 18:02:55 -0300

    problema de conexao com API resolvido

--- app/src/main/java/com/example/myweatherapp/ApiUrls.kt
+++ app/src/main/java/com/example/myweatherapp/ApiUrls.kt
@@ -3,7 +3,7 @@ package com.example.myweatherapp
 class ApiUrls {
 
     companion object{
-        private var baseUrl = "http://api.openweathermap.org/"
+        private var baseUrl = "https://api.openweathermap.org/"
         private var appId = "&appid=2d06429fbe1b359bc8d42fc55301d37a"
 
         var coordinates = baseUrl + "data/2.5/weather?units=metric&lat=%s&lon=%s" + appId
